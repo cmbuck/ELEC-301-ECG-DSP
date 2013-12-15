7e78a0c262e29fd54240ae26480f164cfe63db7f
Christopher Buck <cmbuck@vt.edu>
2013-12-14 22:07:29 -0600

    Modified extrator to work in more situations

--- ELEC301projEKGextract.m
+++ ELEC301projEKGextract.m
@@ -14,19 +14,27 @@ function [data_array,names]=EKGfiles()
 %length. 
 
 dirname=uigetdir(); %choose directory of choice
+% dirname = 'R:\ELEC 301\sample'
 
 dirlisting=dir(fullfile(dirname)); %get all files in the directory. They are stored
 % in dirlisting as a struct array
 
 %create cell arrays
-data_array=cell(1,length(dirlisting)-2); 
-names=cell(1,length(dirlisting)-2); 
+% data_array=cell(1,length(dirlisting)-2); 
+data_array=cell(1,1); 
+% names=cell(1,length(dirlisting)-2); 
+names=cell(1,1); 
 
-for i=1:length(data_array)
-    y=load(strcat(dirname,'/',dirlisting(i+2).name));
-    dat=y.val; %get the values in y into a vector dat
-    data_array{i}=dat;
-    names{i}=dirlisting(i+2).name;
+count = 1;
+for i=1:length(dirlisting)-2
+%     if (strcompi(dirlisting(i+2).name(length(dirlisting(i+2).name)-3:end),'.mat'))
+    if (dirlisting(i+2).name(length(dirlisting(i+2).name)-3:end)=='.mat')
+        y=load(strcat(dirname,'/',dirlisting(i+2).name));
+        dat=y.val; %get the values in y into a vector dat
+        data_array{count}=dat;
+        names{count}=dirlisting(i+2).name;
+        count = count + 1;
+    end
 end
 return
 
